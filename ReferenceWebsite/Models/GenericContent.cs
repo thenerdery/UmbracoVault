d72f9f474e208d1cef6d78664c3a652d9ab67b47
Ken Sykora <ksykora@nerdery.com>
2014-12-03 11:41:09 -0600

    Added initial test

---
+++ ReferenceWebsite/Models/GenericContent.cs
@@ -0,0 +1,14 @@
+﻿using Nerdery.Umbraco.Vault.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReferenceWebsite.Models
+{
+    [UmbracoEntity(AutoMap=true)]
+    public class GenericContent
+    {
+        public string Content { get; set; }
+    }
+}
