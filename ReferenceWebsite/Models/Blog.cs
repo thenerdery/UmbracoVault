87405baa1c1168e08fd46c555763acbe4732bc5a
Phillip Knauss <pknauss@nerdery.com>
2014-12-09 12:12:23 -0600

    Add blog sample

---
+++ ReferenceWebsite/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nerdery.Umbraco.Vault.Attributes;
+
+namespace ReferenceWebsite.Models
+{
+    [UmbracoMediaEntity(AutoMap = true)]
+    public class BlogEntryViewModel
+    {
+        public string Title { get; set; }
+        public DateTime PostDate { get; set; }
+        
+        [UmbracoRichTextProperty]
+        public string Content { get; set; }
+    }
+    
+}
