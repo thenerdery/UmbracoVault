6de0b129474dbcd8249a5e19701cdd9294eb112e
Phillip Knauss <pknauss@nerdery.com>
2014-12-16 11:49:27 -0600

    Add HtmlString handler and ReferenceWebsite example for it

---
+++ UmbracoVault/TypeHandlers/HtmlStringTypeHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+
+namespace UmbracoVault.TypeHandlers
+{
+    /// <summary>
+    /// Responsible for converting HtmlString Types
+    /// </summary>
+    public class HtmlStringTypeHandler : ITypeHandler
+    {
+        private HtmlString Get(string stringValue)
+        {
+            return new HtmlString(stringValue);
+        }
+
+        public object GetAsType<T>(object input)
+        {
+			return Get(input.ToString());
+    	}
+
+        public Type TypeSupported
+        {
+            get { return typeof (HtmlString); }
+        }
+    }
+}
