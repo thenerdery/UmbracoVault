f2efe4ce22e980d918708caead9b2cfe808f6654
Ken Sykora <ksykora@nerdery.com>
2015-02-09 16:22:32 -0600

    Copied umbraco website to be baseline for Umbraco7 tests

---
+++ ReferenceWebsiteUmbraco7/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using ReferenceWebsite.TypeHandlers;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using UmbracoVault;
+using UmbracoVault.Controllers;
+using UmbracoVault.TypeHandlers;
+
+namespace ReferenceWebsite
+{
+    public class Global : UmbracoApplication
+    {
+        protected override void OnApplicationStarting(object sender, System.EventArgs e)
+        {
+            DefaultRenderMvcControllerResolver
+                .Current
+                .SetDefaultControllerType(typeof(VaultDefaultGenericController));
+
+            Vault.RegisterViewModelNamespace("ReferenceWebsite.Models", "ReferenceWebsite");
+            TypeHandlerFactory.Instance.RegisterTypeHandler<LocationIdTypeHandler>();
+        }
+    }
+}
