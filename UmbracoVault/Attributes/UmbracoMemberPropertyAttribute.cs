5730f449e2b8a1ee9e7bdb0a669f49685f1beb4f
Matt Burke <git@mattburkedev.com>
2015-05-17 05:25:20 +0100

    Add attributes for Member mapping (based on Media attributes)

---
+++ UmbracoVault/Attributes/UmbracoMemberPropertyAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UmbracoVault.TypeHandlers;
+
+namespace UmbracoVault.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class UmbracoMemberPropertyAttribute : UmbracoPropertyAttribute
+    {
+        public UmbracoMemberPropertyAttribute()
+        {
+            TypeHandler = new MemberTypeHandler();
+        }
+
+        /// <summary>
+        /// Only needed if the camelCase name of the property is not the same as the umbraco property alias
+        /// </summary>
+        /// <param name="name"></param>
+        public UmbracoMemberPropertyAttribute(string name)
+            : base(name)
+        {
+            TypeHandler = new MemberTypeHandler();
+        }
+    }
+}
