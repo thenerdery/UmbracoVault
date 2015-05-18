007ce09b4a02d2bf94a9e5504fd0b224ea019a0a
Matt Burke <git@mattburkedev.com>
2015-05-18 15:25:11 +0100

    Test in Umbraco 7 reference site

---
+++ ReferenceWebsiteUmbraco7/Models/MembersViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UmbracoVault.Attributes;
+
+namespace ReferenceWebsite.Models
+{
+    [UmbracoEntity]
+    public class MembersViewModel : CmsViewModelBase
+    {
+        [UmbracoMemberProperty("singleMember")]
+        public WebsiteUser User { get; set; }
+    }
+
+    [UmbracoMemberEntity(AutoMap = true)]
+    public class WebsiteUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
