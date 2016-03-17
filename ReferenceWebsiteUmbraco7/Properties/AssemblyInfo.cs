f84bd5804ae6ce19397b5eff2e201356c8267fa0
Ken Sykora <ksykora@nerdery.com>
2016-03-17 10:00:23 -0500

    Fixed Umbraco 7 upgrade issues for reference site

--- ReferenceWebsiteUmbraco7/Properties/AssemblyInfo.cs
+++ ReferenceWebsiteUmbraco7/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ReferenceWebsite")]
+[assembly: AssemblyTitle("ReferenceWebsiteUmbraco7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ReferenceWebsite")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("ReferenceWebsiteUmbraco7")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("13ad58cf-f908-4e67-8179-278cead372a3")]
+[assembly: Guid("27deca8f-8c8e-411c-8f3e-49f7f51dacb9")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ ReferenceWebsiteUmbraco7_/Models/MembersViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Umbraco.Core.Models;
+
+using UmbracoVault.Attributes;
+
+namespace ReferenceWebsite.Models
+{
+    [UmbracoEntity]
+    public class MembersViewModel : CmsViewModelBase
+    {
+        [UmbracoMemberProperty("singleMember")]
+        public WebsiteUser User { get; set; }
+    }
+
+    [UmbracoMemberEntity(AutoMap = true)]
+    public class WebsiteUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public IMember MemberContent { get; set; }
+
+        public bool Approved
+        {
+            get { return MemberContent.IsApproved; }
+        }
+    }
+}
