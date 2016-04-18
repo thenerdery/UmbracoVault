66fc2a8b62a138989ff6a6a88c25072d1fcd36d3
Andrew Morger <amorger@nerdery.com>
2016-04-18 09:21:36 -0500

    Added Umbraco and Vault references.  Created Core Umbraco Application classes and hooked them into Owin.

---
+++ ReferenceApiWeblessUmbraco/App_Start/OwinStartup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using ReferenceApiWeblessUmbraco.Application;
+using ReferenceApiWeblessUmbraco.App_Start;
+
+[assembly: OwinStartup(typeof(OwinStartup))]
+
+namespace ReferenceApiWeblessUmbraco.App_Start
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            StartUmbracoContext();
+        }
+
+        private void StartUmbracoContext()
+        {
+            var application = new ReferenceApiApplicationBase();
+            application.Start(application, new EventArgs());
+        }
+    }
+}
