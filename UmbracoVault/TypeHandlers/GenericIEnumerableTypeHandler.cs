877495e1b4a14fb6575aeed1bff8b15191da6963
Kaleb Luhman <kluhman@nerdery.com>
2017-06-08 12:04:26 -0500

    fixed issue with generic enumerable handler

--- UmbracoVault/TypeHandlers/GenericIEnumerableTypeHandler.cs
+++ UmbracoVault/TypeHandlers/GenericIEnumerableTypeHandler.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
+using Umbraco.Core.Models;
+
 using UmbracoVault.Collections;
 
 namespace UmbracoVault.TypeHandlers
@@ -12,6 +16,29 @@ namespace UmbracoVault.TypeHandlers
 	{
 		public object GetAsType<T>(object input)
         {
+            if (input == null)
+            {
+                return null;
+            }
+
+            var publishedContent = input as IEnumerable<IPublishedContent>;
+            if (publishedContent != null)
+            {
+                return publishedContent.Select(c =>
+                {
+                    try
+                    {
+                        return Vault.Context.IsMediaRequest<T>()
+                            ? Vault.Context.GetMediaById<T>(c.Id)
+                            : Vault.Context.GetContentById<T>(c.Id);
+                    }
+                    catch (ArgumentException)
+                    {
+                        return default(T);
+                    }
+                }).ToList();
+            }
+
 			var collection = new ExternalIteratorEnumerable<T>(input.ToString(), idString =>
 			    {
 			        try
